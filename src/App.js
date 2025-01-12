90366e4ac69f4825231f1605a20a54ae38fffc65
Mateusz Jaracz <mateuszj.aracz@telenorlinx.com>
2025-01-12 13:32:29 +0100

    Firs version of app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState('Wall-E');
+
+  function handleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  let message;
+  if (title.length < 5) {
+    message = "Tutuł jest za krótki. Nagrywają takie filmy?";
+  } else if (title.length < 15) {
+    message = "Tytuł jest ekstra, w sam raz na plakat przed kinem!";
+  } else {
+    message = "Tytuł jest za długi, nikt tego nie zapamięta.";
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My favourite movies to watch</h1>
+      <h2>My favourite movie for today is {title}</h2>
+      <input type="text" value={title} onChange={handleChange} />
+      <div>{message}</div>
     </div>
   );
 }
